6bb5399957080dae2739e9191e5f7deb32288b83
mengshouer <mengshouer@outlook.com>
2021-09-17 17:23:50 +0800

    use FullPath

--- cron_run.js
+++ cron_run.js
@@ -1,7 +1,7 @@
 const schedule = require('node-schedule');
 const fs = require('fs');
 const { exec } = require('child_process');
-const { logspath } = require('./cron_config.js');
+const { logspath, mainDir } = require('./cron_config.js');
 
 // 查看日志目录是否存在,如果不存在,先创建
 const fileExists = fs.existsSync(logspath);
@@ -15,8 +15,8 @@ const rawdata = fs.readFileSync('cron_data.json');
 const cron_data = JSON.parse(rawdata);
 Object.keys(cron_data).forEach(key => {
   schedule.scheduleJob(cron_data[key], function(){
-    let logfile = logspath + key + '.log';
-    exec(`node ${key}`, { cwd: './jdmain' }, (error, stdout, stderr) => {
+    let logfile = `${logspath}/${key}.log`;
+    exec(`node ${key}`, { cwd: `${mainDir}/` }, (error, stdout, stderr) => {
       if (error) {
         console.error(`exec error: ${error}`);
         return;
